29f463373f5f97dae575eef0c906b6b5f6129974
Ramana1205 <gorlaramana05@gmail.com>
2026-05-09 13:11:32 +0530

    adding one

---
+++ day-7/addding1.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class addding1 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter  number");
+        int a = sc.nextInt();
+        int res=-(~a);
+        System.out.println(res);
+    }
+    
+}
