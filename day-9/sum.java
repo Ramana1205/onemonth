1aa0b8a3fe9570ce065df7b98c225551eaf54f21
Ramana1205 <gorlaramana05@gmail.com>
2026-05-21 11:35:17 +0530

    sum of n natural numbers using recursion

---
+++ day-9/sum.java
@@ -0,0 +1,14 @@
+
+
+public class sum {
+    public static int sumn(int n){
+        if(n==1){
+            return 1;
+        }
+        return(n+sumn(n-1));
+    }
+    public static void main(String[] args) {
+        System.out.println(sumn(5));
+        
+    }
+}
