da866abd6b5e27b5e6c50646fbbbf90ae9d98a0a
Ramana1205 <gorlaramana05@gmail.com>
2026-05-24 22:09:08 +0530

    tower of hanoi using recursion

---
+++ day-12/towerofhanoi.java
@@ -0,0 +1,21 @@
+public class towerofhanoi {
+    public static void towerOfHanoi(int n, String src, String helper, String dest) {
+
+        if (n == 0) {
+            return;
+        }
+
+        towerOfHanoi(n - 1, src, dest, helper);
+
+        System.out.println("Move " + n + " from " + src + " to " + dest);
+
+        towerOfHanoi(n - 1, helper, src, dest);
+    }
+
+    public static void main(String[] args) {
+
+        int n = 3;
+
+        towerOfHanoi(n, "A", "B", "C");
+    }
+}
