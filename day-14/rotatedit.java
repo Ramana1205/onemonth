3ef33fca1c0b32eb20ee5c292b4821b431d6e773
Ramana1205 <gorlaramana05@gmail.com>
2026-06-03 08:26:57 +0530

    more optimised than earlier iterative approach for rotated sorted array seardh

--- day-14/rotatedit.java
+++ day-14/rotatedit.java
@@ -2,33 +2,32 @@
 
 public class rotatedit {
     public static int search(int[] arr,int s,int e,int t){
-        int flag=0;
+    
        while(s<=e){
             int mid=s+(e-s)/2;
             if(arr[mid]==t){
-                flag=mid;
-                break;
+                return mid;
             }
-            if(arr[s]<arr[mid]){
-                if(arr[s]<=t && arr[mid]>=t){
+            if(arr[s]<=arr[mid]){
+                if(arr[s]<=t && arr[mid]>t){
                     e=mid-1;
                 }else{
                     s=mid+1;
                 }
             }else{
-                if(arr[mid]<=t && arr[e]>=t){
+                if(arr[mid]<t && arr[e]>=t){
                     s=mid+1;
                 }else{
                     e=mid-1;
                 }
             }
     }
-    return flag;
+    return -1;
 
     }
     public static void main(String[] args) {
         int arr[]={5,6,7,8,0,1,2,3};
-        System.out.println(search(arr,0,arr.length-1,0));
+        System.out.println(search(arr,0,arr.length-1,4));
         
     }
     
